5c44bd5780440000d859c30646a4453cd106d453
Vardhan Varma <vardhan@first.vardhan.net>
2013-03-19 19:07:22 +0530

    creating data foldre

--- gen.sh
+++ gen.sh
@@ -4,6 +4,8 @@
 
 echo Generating binary files of many sizes in static/data
 
+mkdir -p static/data
+
 dd if=/dev/urandom of=static/data/1MB.bin bs=1048576 count=1
 dd if=/dev/urandom of=static/data/5MB.bin bs=1048576 count=5
 dd if=/dev/urandom of=static/data/10MB.bin bs=1048576 count=10
